7155422278194c63b353e69ce9230ca059ac3cb2
mino0310 <bewriter310@gmail.com>
2021-10-10 14:11:39 +0900

    데이터 베이스 스키마 자동 생성

--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -3,12 +3,14 @@ package hellojpa;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
+// @Entity 가 붙으면 이 클래스는 JPA가 관리하는 것이고 DB의 테이블과 매핑을 해서 사용한다고 보면 된다.
 @Entity
 public class Member {
 
     @Id
     private Long id;
     private String name;
+    private int age;
 
     public Member() {
     }
