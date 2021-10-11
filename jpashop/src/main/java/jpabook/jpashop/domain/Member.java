a96bd3a684016f2843140d7d9a10acae57d188f2
mino0310 <bewriter310@gmail.com>
2021-10-12 08:45:32 +0900

    일대다

--- jpashop/src/main/java/jpabook/jpashop/domain/Member.java
+++ jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -15,7 +15,7 @@ public class Member {
     private String street;
     private String zipcode;
 
-    @OneToMany(mappedBy = "member")
+    @OneToMany(mappedBy= "member")
     private List<Order> orders = new ArrayList<>();
 
     public Long getId() {
