10df881e110409413cbe5c2af87416e5b0bcbe43
mino0310 <bewriter310@gmail.com>
2021-10-20 20:08:10 +0900

    JOIN

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -14,16 +14,24 @@ public class JpaMain {
 
         try {
 
+            Team team = new Team();
+            team.setName("teamA");
+            em.persist(team);
+
             Member member = new Member();
-            member.setUsername("kim");
+            member.setUsername("member");
             member.setAge(10);
+
+            member.setTeam(team);
+
             em.persist(member);
 
             em.flush();
             em.clear();
 
-            List<Member> result = em.createQuery("select m from Member m order by m.age desc", Member.class)
-                    .setFirstResult(1).setMaxResults(10).getResultList();
+            String query = "select m from Member m inner join m.team t";
+            List<Member> result = em.createQuery(query, Member.class)
+                    .getResultList();
             System.out.println("result.size() = " + result.size());
             for (Member member1 : result) {
                 System.out.println("member1 = " + member1);
--- jpql/src/main/java/jpql/Member.java
+++ jpql/src/main/java/jpql/Member.java
@@ -14,6 +14,11 @@ public class Member {
     @JoinColumn(name = "TEAM_ID")
     private Team team;
 
+    public void changeTeam(Team team) {
+        this.team = team;
+        team.getMembers().add(this);
+    }
+
     public Long getId() {
         return id;
     }
@@ -36,6 +41,14 @@ public class Member {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    public Team getTeam() {
+        return team;
+    }
+
+    public void setTeam(Team team) {
+        this.team = team;
     }
 
     @Override
