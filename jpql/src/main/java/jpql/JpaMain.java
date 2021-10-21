2d3131cfa92bae47a789b4950e33a8a02e88bc98
mino0310 <bewriter310@gmail.com>
2021-10-21 22:18:09 +0900

    페치 조인

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -15,28 +15,52 @@ public class JpaMain {
 
         try {
 
-            Team team = new Team();
-            em.persist(team);
+            Team teamA = new Team();
+            teamA.setName("팀A");
+            em.persist(teamA);
 
-            Member member = new Member();
-            member.setUsername("관리자1");
-            member.setTeam(team);
-            em.persist(member);
+            Team teamB = new Team();
+            teamB.setName("팀B");
+            em.persist(teamB);
+
 
             Member member1 = new Member();
-            member1.setUsername("관리자2");
-            member1.setTeam(team);
+            member1.setUsername("회원1");
+            member1.setTeam(teamA);
             em.persist(member1);
+
+            Member member2 = new Member();
+            member2.setUsername("회원2");
+            member2.setTeam(teamA);
+            em.persist(member2);
+
+            Member member3 = new Member();
+            member3.setUsername("회원3");
+            member3.setTeam(teamB);
+            em.persist(member3);
 
             em.flush();
             em.clear();
 
 
-            String query = "select t.members.size from Team t";
-            Integer result = em.createQuery(query, Integer.class).getSingleResult();
+            String query = "select t from Team t";
+            List<Team> resultList = em.createQuery(query, Team.class).setFirstResult(0).setMaxResults(2).getResultList();
 
-            System.out.println("result = " + result);
+            for (Team team : resultList) {
+                System.out.println("team = " + team.getName() + " | members = " + team.getMembers().size());
+                for (Member member : team.getMembers()) {
+                    System.out.println("member = " + member);
+                }
+            }
+/*            for (Member member : resultList) {
+                System.out.println("member = " + member.getUsername() + ", " + member.getTeam().getName());
+                // 페치 조인을 사용하지 않으면..
+                // 회원1, 팀A (SQL)
+                // 회원2, 팀A (1차 캐시)
+                // 회원3, 팀B (SQL)
+                // 회원 100명을 조회한다면 최악의 경우엔 100번의 쿼리를 날려야 함 -> N + 1 문제. 1 + N문제라 보는 것이 합당. 첫번째 쿼리를 날려 얻은 결과의 각각에 대해서 N번의 쿼리를 날리는 것. 즉시로딩을 하든 지연 로딩을 하든 무조건 발생할 수밖에 없음.
 
+            }*/
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
--- jpql/src/main/java/jpql/Team.java
+++ jpql/src/main/java/jpql/Team.java
@@ -1,5 +1,7 @@
 package jpql;
 
+import org.hibernate.annotations.BatchSize;
+
 import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
@@ -11,6 +13,7 @@ public class Team {
     private Long id;
     private String name;
 
+    @BatchSize(size = 100 )
     @OneToMany(mappedBy = "team")
     private List<Member> members = new ArrayList<>();
 
