ba62cc00eb85625f9ea1ea3ab3aaf36f4ff3d968
mino0310 <bewriter310@gmail.com>
2021-10-21 22:40:04 +0900

    엔티티 직접 사용

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -43,24 +43,14 @@ public class JpaMain {
             em.clear();
 
 
-            String query = "select t from Team t";
-            List<Team> resultList = em.createQuery(query, Team.class).setFirstResult(0).setMaxResults(2).getResultList();
+            String query = "select m from Member m where m.team = :team";
 
-            for (Team team : resultList) {
-                System.out.println("team = " + team.getName() + " | members = " + team.getMembers().size());
-                for (Member member : team.getMembers()) {
-                    System.out.println("member = " + member);
-                }
+            List<Member> members = em.createQuery(query, Member.class).setParameter("team", teamB).getResultList();
+
+            for (Member member : members) {
+                System.out.println("member = " + member);
             }
-/*            for (Member member : resultList) {
-                System.out.println("member = " + member.getUsername() + ", " + member.getTeam().getName());
-                // 페치 조인을 사용하지 않으면..
-                // 회원1, 팀A (SQL)
-                // 회원2, 팀A (1차 캐시)
-                // 회원3, 팀B (SQL)
-                // 회원 100명을 조회한다면 최악의 경우엔 100번의 쿼리를 날려야 함 -> N + 1 문제. 1 + N문제라 보는 것이 합당. 첫번째 쿼리를 날려 얻은 결과의 각각에 대해서 N번의 쿼리를 날리는 것. 즉시로딩을 하든 지연 로딩을 하든 무조건 발생할 수밖에 없음.
 
-            }*/
             tx.commit();
         } catch (Exception e) {
             tx.rollback();
